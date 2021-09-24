ee85ac08b8725e825a06bff2293416196c2fbd32
Daniel Farias <dan.tfarias@gmail.com>
2021-09-24 01:18:44 -0300

    Desenvolvimento página checkout

--- _js/script.js
+++ _js/script.js
@@ -1,3 +1,4 @@
+
 function googleTranslateElementInit2() {new google.translate.TranslateElement({pageLanguage: 'pt',autoDisplay: false}, 'google_translate_element2');}
 
 eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 7(a,b){n{4(2.9){3 c=2.9("o");c.p(b,f,f);a.q(c)}g{3 c=2.r();a.s(\'t\'+b,c)}}u(e){}}6 h(a){4(a.8)a=a.8;4(a==\'\')v;3 b=a.w(\'|\')[1];3 c;3 d=2.x(\'y\');z(3 i=0;i<d.5;i++)4(d[i].A==\'B-C-D\')c=d[i];4(2.j(\'k\')==E||2.j(\'k\').l.5==0||c.5==0||c.l.5==0){F(6(){h(a)},G)}g{c.8=b;7(c,\'m\');7(c,\'m\')}}',43,43,'||document|var|if|length|function|GTranslateFireEvent|value|createEvent||||||true|else|doGTranslate||getElementById|google_translate_element2|innerHTML|change|try|HTMLEvents|initEvent|dispatchEvent|createEventObject|fireEvent|on|catch|return|split|getElementsByTagName|select|for|className|goog|te|combo|null|setTimeout|500'.split('|'),0,{}))
@@ -71,6 +72,7 @@ function checkEmails() {
     if (m1 != m2) {
         document.getElementById('mailConfirm').style.color = 'red';
         document.getElementById('mailConfirm').value = 'Verifique o e-mail digitado';
+        mailsChecked = false;
     }
     else {
         mailsChecked = true;
@@ -84,7 +86,8 @@ function checkName() {
     }
     else {
         document.getElementById('nameInput').style.color = 'red';
-        document.getElementById('nameInput').value = 'Verifique o preenchimento do nome';       
+        document.getElementById('nameInput').value = 'Verifique o preenchimento do nome';
+        nameChecked = false;       
     }
 }
 
@@ -92,12 +95,13 @@ function checkPhone() {
     var phone = document.getElementById('telInput').value;
     if (isNaN(phone) || phone == "") {
         document.getElementById('telInput').style.color = 'red';
-        document.getElementById('telInput').value = 'Verifique o preenchimento do telefone';     
+        document.getElementById('telInput').value = 'Verifique o preenchimento do telefone';
+        phoneChecked = false;     
     }
     else {
         phoneChecked = true;
     }
-}
+} 
 
 function clearField(field) {
     if (document.getElementById(field).style.color == 'red') {
@@ -106,4 +110,3 @@ function clearField(field) {
     }
 
 }
-
