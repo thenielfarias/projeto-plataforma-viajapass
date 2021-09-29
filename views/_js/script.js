be2de7f5ffbfb29758b6d0747be9a040bae0ef89
Daniel Farias <dan.tfarias@gmail.com>
2021-09-29 18:14:20 -0300

    Melhorias checkout

--- views/_js/script.js
+++ views/_js/script.js
@@ -3,14 +3,6 @@ function googleTranslateElementInit2() {new google.translate.TranslateElement({p
 
 eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 7(a,b){n{4(2.9){3 c=2.9("o");c.p(b,f,f);a.q(c)}g{3 c=2.r();a.s(\'t\'+b,c)}}u(e){}}6 h(a){4(a.8)a=a.8;4(a==\'\')v;3 b=a.w(\'|\')[1];3 c;3 d=2.x(\'y\');z(3 i=0;i<d.5;i++)4(d[i].A==\'B-C-D\')c=d[i];4(2.j(\'k\')==E||2.j(\'k\').l.5==0||c.5==0||c.l.5==0){F(6(){h(a)},G)}g{c.8=b;7(c,\'m\');7(c,\'m\')}}',43,43,'||document|var|if|length|function|GTranslateFireEvent|value|createEvent||||||true|else|doGTranslate||getElementById|google_translate_element2|innerHTML|change|try|HTMLEvents|initEvent|dispatchEvent|createEventObject|fireEvent|on|catch|return|split|getElementsByTagName|select|for|className|goog|te|combo|null|setTimeout|500'.split('|'),0,{}))
 
-function calc_total() {
-    var qtd = parseInt(document.getElementById('qtd').value)
-    tot = qtd * 198
-    totDec = tot.toFixed(2)
-    totVir = totDec.replace(".",",")
-    document.getElementById('tot').value = totVir
-}
-
 function checkName() {
     var nameInput = document.getElementById('nameInput').value;
     var name = nameInput.replace(/\s/g, '');
@@ -27,7 +19,7 @@ function checkName() {
 function isEmail() {
     var email = document.getElementById('mailInput').value;
     var re = /\S+@\S+\.\S+/;
-    validMail = re.test(email);
+    var validMail = re.test(email);
     if (validMail) {
         return true;
     }
@@ -80,10 +72,10 @@ function closeModal(modal) {
 }
 
 function calc_total() {
-    qtd = parseInt(document.getElementById('qtd').value)
-    tot = qtd * 198
-    totDec = tot.toFixed(2)
-    totVir = totDec.replace(".",",")
+    var qtd = parseInt(document.getElementById('qtd').value)
+    var tot = qtd * 198
+    var totDec = tot.toFixed(2)
+    var totVir = totDec.replace(".",",")
     document.getElementById('tot').value = totVir
 }
 
@@ -91,3 +83,4 @@ function getQtd() {
     var getInput = parseInt(document.getElementById('qtd').value);
     localStorage.setItem("qtd",getInput);
 }
+
