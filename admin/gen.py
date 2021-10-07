e4d7863d0b82a223f077f584a43b6c5e9045d588
Daniel Farias <dan.tfarias@gmail.com>
2021-10-07 18:02:34 -0300

    Update gen.py

--- admin/gen.py
+++ admin/gen.py
@@ -7,6 +7,7 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import re
 
 def generate():
     print('Digite os dados do titular para gerar a compra')
